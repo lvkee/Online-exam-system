7fefdb1ed95dc5883ef0a3b83729e50a5472e1df
14194801786 <1986985788@qq.com>
2022-03-21 10:25:26 +0800

    first commit

---
+++ target/userLogin/admin/javascript/pages/login.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('#entry').click(function () {
+        if ($('#adminName').val() == '') {
+            $('.mask,.dialog').show();
+            $('.dialog .dialog-bd p').html('请输入管理员账号');
+        } else if ($('#adminPwd').val() == '') {
+            $('.mask,.dialog').show();
+            $('.dialog .dialog-bd p').html('请输入管理员密码');
+        } else {
+            $('.mask,.dialog').hide();
+            // location.href = 'index.html';
+            var xhr = new XMLHttpRequest();
+            // we defined the xhr
+            xhr.onreadystatechange = function () {
+                if (this.readyState !== 4) return;
+                if (this.status === 200) {
+                    var data = JSON.parse(this.responseText);
+                    // we get the returned data
+                }
+                // end of state change: it can be after some time (async)
+            };
+            xhr.open('POST', "/userLogin/user/checkLogin?username=" + $('#adminName').val() + "&password=" + $('#adminPwd').val(), true);
+            xhr.send();
+        }
+    });
+});
